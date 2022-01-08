4bc54502da5fa8d0f7b2f867642afa93888b1156
Jejshhs <92094971+Jejshhs@users.noreply.github.com>
2022-01-08 13:56:47 +0530

    Update __init__.py

--- alita/__init__.py
+++ alita/__init__.py
@@ -68,11 +68,11 @@ except Exception as ef:
     sysexit(1)
 
 LOGGER.info("------------------------")
-LOGGER.info("|      Alita_Robot     |")
+LOGGER.info("|      Kiramanagementbot     |")
 LOGGER.info("------------------------")
 LOGGER.info(f"Version: {Config.VERSION}")
 LOGGER.info(f"Owner: {str(Config.OWNER_ID)}")
-LOGGER.info("Source Code: https://github.com/DivideProjects/Alita_Robot\n")
+LOGGER.info("Updtes: https://t.me/phoenix_empire\n")
 
 # Account Related
 BOT_TOKEN = Config.BOT_TOKEN
